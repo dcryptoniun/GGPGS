058ad1d575458effd6beb038e0bdac1216f86d47
Mayank Meena <dcryptoniun@gmail.com>
2025-03-06 15:26:37 +0530

    added doc for adding gameid in godot editor

--- plugin/demo/main.gd
+++ plugin/demo/main.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
-@onready var title : Label = $Title
-@onready var playerinfo : Label = $PlayerInfo
+@onready var title : Label = %Title
+@onready var testlabel : Label = %TestLabel
+@onready var playerinfo : Label = %PlayerInfo
 
 var _plugin_name = "GGPGS"
 var ggpgs_plugin
@@ -10,6 +11,7 @@ func _ready() -> void:
 	if Engine.has_singleton(_plugin_name):
 		ggpgs_plugin = Engine.get_singleton(_plugin_name)
 		ggpgs_plugin.connect("userDetailsSignal",_on_userDetailsSignal)
+		
 	else:
 		printerr("ggpgs plugin not found")
 		
--- plugin/export_scripts_template/export_plugin.gd
+++ plugin/export_scripts_template/export_plugin.gd
@@ -3,18 +3,30 @@ extends EditorPlugin
 
 # A class member to hold the editor export plugin during its lifecycle.
 var export_plugin : AndroidExportPlugin
+var _dock : Node
 
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	export_plugin = AndroidExportPlugin.new()
 	add_export_plugin(export_plugin)
+	_add_docks()
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	remove_export_plugin(export_plugin)
+	_remove_docks()
 	export_plugin = null
 
+
+func _add_docks() -> void:
+	var dock_name := &"GGPGS"
+	_dock = preload("res://addons/GGPGS/ggpgs_dock.tscn").instantiate()
+	add_control_to_bottom_panel(_dock, dock_name)
+
+func _remove_docks() -> void:
+	remove_control_from_bottom_panel(_dock)
+	_dock.free()
 
 class AndroidExportPlugin extends EditorExportPlugin:
 	# TODO: Update to your plugin's name.
@@ -37,6 +49,13 @@ class AndroidExportPlugin extends EditorExportPlugin:
 			return PackedStringArray(["com.google.android.gms:play-services-games-v2:20.1.2"])
 		else:
 			return PackedStringArray(["com.google.android.gms:play-services-games-v2:20.1.2"])
+
+	func _get_android_manifest_application_element_contents(platform: EditorExportPlatform, debug: bool) -> String:
+		if not _supports_platform(platform):
+			return ""
+		
+		return "<meta-data android:name=\"com.google.android.gms.games.APP_ID\" android:value=\"@string/game_services_project_id\"/>"
+
 
 	func _get_name():
 		return _plugin_name
